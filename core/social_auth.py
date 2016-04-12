553d3e40e1f97d7d1ae091e83ebf4c0e4ad1cd81
Ali Hallaji <ali.hallaji1@gmail.com>
2016-04-12 12:25:26 +0430

    Add Social authenticate 48%

---
+++ core/social_auth.py
@@ -0,0 +1,45 @@
+# Python Import
+from allauth.account.signals import user_logged_in
+
+# Django Import
+from django.dispatch import receiver
+
+
+@receiver(user_logged_in)
+def social_auth_handler(request, user, sociallogin=None, **kwargs):
+    '''
+    When a social account is created or login successfully and this
+    signal is received, django-allauth passes in the sociallogin param,
+    giving access to metadata on the remote account, e.g.:
+
+    sociallogin.account.provider  # e.g. 'twitter'
+    sociallogin.account.get_avatar_url()
+    sociallogin.account.get_profile_url()
+    sociallogin.account.extra_data['screen_name']
+
+    See the socialaccount_socialaccount table for more
+    in the 'extra_data' field.
+    '''
+
+    print "#########################################"
+    print sociallogin.account.extra_data
+    print "#########################################"
+
+    if sociallogin:
+        # Extract first / last names from social nets and store on User record
+        if sociallogin.account.provider == 'twitter':
+            name = sociallogin.account.extra_data['name']
+            first_name = name.split()[0]
+            last_name = name.split()[1]
+            email = sociallogin.account.extra_data['email']
+
+        if sociallogin.account.provider == 'facebook':
+            first_name = sociallogin.account.extra_data['first_name']
+            last_name = sociallogin.account.extra_data['last_name']
+            email = sociallogin.account.extra_data['email']
+
+        if sociallogin.account.provider == 'google':
+            first_name = sociallogin.account.extra_data['given_name']
+            last_name = sociallogin.account.extra_data['family_name']
+            email = sociallogin.account.extra_data['email']
+
