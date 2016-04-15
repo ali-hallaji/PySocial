74b296f2f138acebe82cef76f448e5e8e886f6a7
Milad Meidanshahi <miladmeidanshahi@gmail.com>
2016-04-15 22:39:23 +0430

    search modal added and some part edited

---
+++ static/main/js/script.js
@@ -0,0 +1,16 @@
+// $('.uk-modal').on({
+
+//     'show.uk.modal': function(){
+//         $('body').css({
+//             'padding-left': '',
+//             'padding-right': '15px'
+//         });
+//     },
+
+//     'hide.uk.modal': function(){
+//         $('body').css({
+//             'padding-left': '',
+//             'padding-right': '15px'
+//         });
+//     }
+// });
