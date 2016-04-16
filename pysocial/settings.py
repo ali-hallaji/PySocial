561dba28638063e027b1b6de03119b42e2b6272e
Ali Hallaji <ali.hallaji1@gmail.com>
2016-04-16 17:45:48 +0430

    Add context processor for user data4

--- pysocial/settings.py
+++ pysocial/settings.py
@@ -101,7 +101,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'pysocial.context_processor.get_user_data',
+                'pysocial.context_processor.get_user_data',
             ],
         },
     },
