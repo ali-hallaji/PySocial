c9ff73bcbb08f83f77dc1c3cb981255116b9856b
Ali Hallaji <ali.hallaji1@gmail.com>
2016-04-17 15:31:51 +0430

    Add boxs into home page

--- pysocial/views.py
+++ pysocial/views.py
@@ -10,18 +10,24 @@ from core import cursor
 
 def home(request):
     kwargs = {}
+
+    # Get data from DB
     news = cursor.home.find_one({'kind': 'news'})
     what = cursor.home.find_one({'kind': 'what'})
     roadmap = cursor.home.find({'kind': 'roadmap'}).sort('order', ASCENDING)
+    boxs = cursor.box.find().sort('order', ASCENDING)
 
     if news:
-        kwargs['news'] = news['news']
+        kwargs['news'] = news['body']
 
     if what:
         kwargs['what'] = what['body']
 
     if roadmap:
         kwargs['roadmap'] = list(roadmap)
+
+    if boxs:
+        kwargs['boxs'] = list(boxs)
 
     return render(request, 'home.html', kwargs)
 
