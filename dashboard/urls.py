e1fcbb3534b0b56df4933544014c6c7ab35ca72f
Ali Hallaji <ali.hallaji1@gmail.com>
2016-04-17 23:22:36 +0430

    Add Lesson

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 # PySocial import
 from views import content
+from views import lesson
 
 
 urlpatterns = [
@@ -10,5 +11,10 @@ urlpatterns = [
         r'^content/(?P<dashboard>[\w\d]+)/(?P<_id>[\w\d]+)/$',
         content,
         name="content"
+    ),
+    url(
+        r'^lesson/(?P<dashboard>[\w\d]+)/(?P<_id>[\w\d]+)/$',
+        lesson,
+        name="lesson"
     ),
 ]
