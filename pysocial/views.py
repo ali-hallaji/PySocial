82f4f8dab6d4a4d9bfb6986f922010274514b9ab
Ali Hallaji <ali.hallaji1@gmail.com>
2016-04-25 11:30:02 +0430

    Add last lesson

--- pysocial/views.py
+++ pysocial/views.py
@@ -1,10 +1,12 @@
 # Python Import
 from pymongo import ASCENDING
+from pymongo import DESCENDING
 
 # Django Import
 from django.shortcuts import render
 
 # PySocial Import
+from settings import last_lesson_qty
 from core import cursor
 
 
@@ -16,6 +18,7 @@ def home(request):
     what = cursor.home.find_one({'kind': 'what'})
     roadmap = cursor.home.find({'kind': 'roadmap'}).sort('order', ASCENDING)
     boxs = cursor.box.find().sort('order', ASCENDING)
+    last_lesson = cursor.lessons.find().sort('created', DESCENDING)
 
     if news:
         kwargs['news'] = news['body']
@@ -28,6 +31,9 @@ def home(request):
 
     if boxs:
         kwargs['boxs'] = list(boxs)
+
+    if last_lesson:
+        kwargs['last_lesson'] = list(last_lesson.limit(last_lesson_qty))
 
     return render(request, 'home.html', kwargs)
 
