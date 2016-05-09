57b640ed16b6adc2e107478d323af46ef425f8ec
Ali Hallaji <ali.hallaji1@gmail.com>
2016-05-09 17:14:01 +0430

    Add add_forum views

--- forum/forms.py
+++ forum/forms.py
@@ -1,11 +1,3 @@
 from django import forms
 
 
-class ForumForm(forms.Form):
-    sort = forms.IntegerField(label='sort')
-    description = forms.CharField(
-        label='description',
-        widget=forms.Textarea()
-    )
-    title = forms.CharField(label='title')
-    title_en = forms.CharField(label='title en')
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,5 @@
 # Python Import
+import os
 import re
 
 from pymongo import ASCENDING
@@ -6,32 +7,19 @@ from pymongo import DESCENDING
 
 # Django Import
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 # PySocial Import
 from core import cursor
-from forms import ForumForm
+from core.func_tools import handle_uploaded_file
+from pysocial.settings import BASE_DIR
 
 
 def show_forum(request):
-    kwargs = {}
-    kwargs['forum'] = list(cursor.forum.find().sort('sort', ASCENDING))
-    return render(request, 'forum/show_forum.html', kwargs)
+    pass
 
 
-@login_required
-def add_forum(request):
-    kwargs = {}
 
-    if request.method == 'POST':
-        kwargs['form'] = ForumForm(request.POST)
-
-        if kwargs['form'].is_valid():
-            data = kwargs['form'].cleaned_data
-
-    else:
-        kwargs['form'] = ForumForm()
-
-    return render(request, 'forum/forum.html', kwargs)
