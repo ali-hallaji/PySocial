9a06a71cfb0c102ac2b0a8a6b0522832b6f52c64
Milad Meidanshahi <miladmeidanshahi@gmail.com>
2016-05-27 23:33:53 +0430

    manager switch to ltr - many forms and tables edito to new design - new button style

--- static/js/manager_script.js
+++ static/js/manager_script.js
@@ -1,15 +1,4 @@
-$(document).ready(function() {
+Waves.attach('.btn', ['waves-button', 'waves-float']);
+Waves.init();
 
-});
 $('table').DataTable();
-$('div#DataTables_Table_0_paginate')
-    .children('ul')
-    .attr('class', 'uk-pagination uk-pagination-left');
-
-$('#DataTables_Table_0_previous')
-    .find('.uk-icon-angle-left')
-    .attr('class', 'uk-icon-angle-right');
-
-$('#DataTables_Table_0_next')
-    .find('.uk-icon-angle-right')
-    .attr('class', 'uk-icon-angle-left');
