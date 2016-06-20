8633fd7445a1645c2b1171694b610cdfe4557583
Ali Hallaji <ali.hallaji1@gmail.com>
2016-06-20 15:58:44 +0430

    Add forum page

--- forum/views.py
+++ forum/views.py
@@ -1,22 +1,13 @@
 # Python Import
-import os
-import re
-
-from pymongo import ASCENDING
-from pymongo import DESCENDING
 
 # Django Import
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 
 # PySocial Import
 from core import cursor
-from core.func_tools import handle_uploaded_file
-from pysocial.settings import BASE_DIR
 
 
 def show_forum(request):
-    return render(request, 'forum/show_forum.html')
+    kwargs = {}
+    kwargs['forum'] = list(cursor.forum.find())
+    return render(request, 'forum/show_forum.html', kwargs)
